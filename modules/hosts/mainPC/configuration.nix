3229f8188d0bdfcc14e9641e790b7a1020a6ed1f
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-05-11 00:48:15 +0200

    flakes

--- modules/hosts/mainPC/configuration.nix
+++ modules/hosts/mainPC/configuration.nix
@@ -88,6 +88,10 @@
       programs.nix-ld.enable = true;
       # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
       # List packages installed in system profile. To search, run:
       # $ nix search wget
