63a18e891893b6ef4865f2ad9c9c579827f4007c
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-05-17 00:36:37 +0200

    no virtual desktop

--- modules/features/desktop/kde/panels.nix
+++ modules/features/desktop/kde/panels.nix
@@ -13,7 +13,7 @@ let
     inherit screen;
     widgets = [
       "org.kde.plasma.kickoff"
-      "org.kde.plasma.pager"
+      # "org.kde.plasma.pager"
       "org.kde.plasma.icontasks"
       "org.kde.plasma.marginsseparator"
       "org.kde.plasma.systemtray"
