56d269943125bfa32699f68df04a73318d01b27a
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-06-21 16:55:07 +0200

    aoe

--- home/apps/neovide/default.nix
+++ home/apps/neovide/default.nix
@@ -3,14 +3,24 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   programs.neovide = {
     enable = true;
     settings = {
       maximized = true;
+      font = {
+        normal = [
+          "Google Sans Code Monospaced"
+          "CaskaydiaCove Nerd Font"
+        ];
+        size = 14;
+      };
     };
   };
 
   # Inject Neovide-specific keymappings and settings into nixvim config
-  programs.nixvim.extraConfigLua = lib.mkIf config.programs.nixvim.enable (builtins.readFile ./neovim-config.lua);
+  programs.nixvim.extraConfigLua = lib.mkIf config.programs.nixvim.enable (
+    builtins.readFile ./neovim-config.lua
+  );
 }
