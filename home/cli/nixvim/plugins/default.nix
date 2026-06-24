abf1ea9e11d22cc5b83ecb5838df39cdeff2c1f9
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-06-24 23:24:43 +0200

    update

--- home/cli/nixvim/plugins/default.nix
+++ home/cli/nixvim/plugins/default.nix
@@ -18,7 +18,7 @@
     ./yazi.nix # file explorer for Neovim
     # ./hardtime.nix # Prevents accidental key presses
     ./neoscroll.nix # smooth scrolling for Neovim
-    ./copilot.nix
+    # ./copilot.nix
     # ./pi.nix # AI coding assistant (pi.dev)
     ./lazygit.nix
     ./neogit.nix
