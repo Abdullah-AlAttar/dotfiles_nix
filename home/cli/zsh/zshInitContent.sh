4abd78544d2a01ef0cf26916ab87bf792290adef
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-06-30 16:41:19 +0200

    update

--- home/cli/zsh/zshInitContent.sh
+++ home/cli/zsh/zshInitContent.sh
@@ -9,3 +9,8 @@ export EDITOR=nvim
 export VISUAL=nvim
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.npm-global/bin:$PATH"
+
+# Git add-commit-push shortcut
+gacp() {
+  git add . && git commit -m "$*" && git push
+}
