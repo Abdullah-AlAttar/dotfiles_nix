7a03b8f438c64003f579bb1e6293b3983dae839c
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2026-08-22 22:33:48 +0200

    fix xfce

--- modules/features/desktop/xfce/system.nix
+++ modules/features/desktop/xfce/system.nix
@@ -1,19 +1,20 @@
-{pkgs, ...}: {
-  services.xserver.desktopManager.xfce = {
-    enable = true;
-    enableScreensaver = false;
-  };
+{...}: {
+  flake.nixosModules.xfceSystem = {pkgs, ...}: {
+    services.xserver.desktopManager.xfce = {
+      enable = true;
+      enableScreensaver = false;
+    };
 
-  # Exclude unnecessary XFCE apps
-  environment.xfce.excludePackages = with pkgs; [
-    xfce.parole        # media player
-    xfce.xfburn        # CD burning
-    xfce.xfce4-mailwatch-plugin
-    xfce.xfce4-screenshooter
-    xfce.xfce4-taskmanager
-    xfce.xfce4-terminal
-    xfce.xfhelp        # help browser
-    xfce.xfmpc         # MPD client
-    xfce.xfce4-power-manager  # using upower directly
-  ];
+    # Exclude unnecessary XFCE apps
+    environment.xfce.excludePackages = with pkgs; [
+      xfce.parole        # media player
+      xfce.xfburn        # CD burning
+      xfce.xfce4-mailwatch-plugin
+      xfce.xfce4-screenshooter
+      xfce.xfce4-taskmanager
+      xfce.xfce4-terminal
+      xfce.xfce4-mpc-plugin  # MPD client
+      xfce.xfce4-power-manager  # using upower directly
+    ];
+  };
 }
